bdd7fc0ec23c6e2ffecfd110ca239ee3e6d46158
ArtG <artem.girda@gmail.com>
2021-01-26 16:45:56 +0200

    product-group/modal in progress

--- src/modules/catalog/pages/product-list/product-list.component.ts
+++ src/modules/catalog/pages/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  @ViewChild('buttonMenuText') buttonMenuText: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fixedBtn(e): void {
+    if (window.pageYOffset >= 300) {
+      this.buttonMenuText.nativeElement.style.display = 'none';
+    } else {
+      this.buttonMenuText.nativeElement.style.display = 'block';
+    }
   }
 
 }
--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,10 +1,22 @@
 import {NgModule} from '@angular/core';
 import {ProductCardComponent} from './components/product-card/product-card.component';
+import {ProductGroupComponent} from './components/product-group/product-group.component';
+import {ProductComponent} from './components/product-group/product/product.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   imports: [],
-  declarations: [ProductCardComponent],
-  exports: [ProductCardComponent]
+  declarations: [
+    ProductCardComponent,
+    ProductGroupComponent,
+    ProductComponent,
+    ModalComponent
+  ],
+  exports: [
+    ProductCardComponent,
+    ProductGroupComponent,
+    ModalComponent
+  ]
 })
 export class SharedModule {
 }
